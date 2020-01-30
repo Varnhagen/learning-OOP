9f9a8ed0025f02fd88ae771df4fafe93091976d7
Varnhagen <marcylian@gmail.com>
2020-01-30 18:22:59 +0100

    declares 'Human' class

---
+++ exercise1.py
@@ -0,0 +1,20 @@
+class Human:
+    name = ""
+    age = None
+    sex = ""
+
+    def say_hello(self):
+        print("Hi! My name is " + self.name)
+    
+    def move(self):
+        if sex = "male":
+            print("*he moves*")
+        else:
+            print("*she moves*")
+
+    def think(self):
+        if age > 2:
+            print("Ok! I will think about it!")
+        else:
+            print("Huh?")
+
