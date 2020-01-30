e0dfdc699578ed0263861143d3958a7746b76983
Varnhagen <marcylian@gmail.com>
2020-01-30 22:10:06 +0100

    working with human class and objects

--- exercise1.py
+++ exercise1.py
@@ -7,14 +7,23 @@ class Human:
         print("Hi! My name is " + self.name)
     
     def move(self):
-        if sex = "male":
+        if self.sex == "male":
             print("*he moves*")
         else:
             print("*she moves*")
 
     def think(self):
-        if age > 2:
+        if self.age > 2:
             print("Ok! I will think about it!")
         else:
             print("Huh?")
 
+citizen1 = Human()
+citizen1.name = "Mike"
+citizen1.age = 23
+citizen1.sex = "male"
+
+print(f"Object - {citizen1.sex}, {citizen1.name}, age: {citizen1.age}")
+citizen1.say_hello()
+citizen1.move()
+citizen1.think()
